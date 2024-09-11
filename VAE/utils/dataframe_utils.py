e7920f5a62d7d660acb9c7269cad8b1735f2878e
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2024-09-11 11:09:26 +0200

    Added Diabetes Data

--- VAE/utils/dataframe_utils.py
+++ VAE/utils/dataframe_utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def split_columns(df: pd.DataFrame, save_path=None):
+def split_columns(df: pd.DataFrame, save_path=None, diabetes_data=False):
     """
     Split the input DataFrame into two parts:
     - all columns except the last one
@@ -13,9 +13,13 @@ def split_columns(df: pd.DataFrame, save_path=None):
     pd.DataFrame, pd.Series: A DataFrame with all columns except the last one, and the last column as a Series.
     """
     # All columns except the last one
+
     all_except_last = df.iloc[:, :-1]
 
     # The last column
-    last_column = df.iloc[:, -1]
+    if diabetes_data:
+        last_column = df.iloc[:, -1].map({"b'tested_positive'": 1, "b'tested_negative'": 0})
+    else:    
+        last_column = df.iloc[:, -1]
     all_except_last.to_csv(save_path, index=False)
     return all_except_last, last_column
--- main.py
+++ main.py
@@ -18,7 +18,9 @@ import argparse
 
 def main(dataset=None, trained_model=None):
 
-    if dataset is None:
+    # Bank Dataset
+
+    if dataset == "bank":
         # The default block of code to run if no dataset is specified
         data = pd.read_csv("data/raw/bank.csv", sep=",")  # Raw Data
         _, label = split_columns(data, save_path="data/interim/bank_no_label.csv")
@@ -55,6 +57,167 @@ def main(dataset=None, trained_model=None):
         print(train_size, val_size, test_size)
 
         # Ensure the directory exists
+        output_dir = f'data/interim/{dataset}_latent/'
+        os.makedirs(output_dir, exist_ok=True)
+
+        # Save X arrays
+        np.save(os.path.join(output_dir, 'X_num_train.npy'), X_train)
+        np.save(os.path.join(output_dir, 'X_num_val.npy'), X_val)
+        np.save(os.path.join(output_dir, 'X_num_test.npy'), X_test)
+        
+        print("Interim Data X Saved in .npy format for Tabddpm")
+
+        # Save y arrays
+        np.save(os.path.join(output_dir, 'y_train.npy'), y_train)
+        np.save(os.path.join(output_dir, 'y_val.npy'), y_val)
+        np.save(os.path.join(output_dir, 'y_test.npy'), y_test)
+        
+        print("Interim Data Y Saved in .npy format for Tabddpm")
+
+        # Make num_feature_size a input parameter
+        num_feature_size, cat_features_size = 13, 0 # These are default considering latent size of each client is 3 in VAE
+        
+        info_file = {
+        "task_type": "binclass",
+        "name": f"{dataset}_latent",
+        "id": f"{dataset}_latent--id",
+        "train_size": train_size,
+        "val_size": val_size,
+        "test_size": test_size,
+        "n_num_features": num_feature_size,
+        "n_cat_features": cat_features_size
+        }
+        
+        os.makedirs(f"data/interim/{dataset}_latent", exist_ok=True)
+        with open(f"data/interim//{dataset}_latent/info.json", "w") as info_writer:
+            json.dump(info_file, info_writer)
+
+        os.makedirs(f"data/external/{dataset}_latent", exist_ok=True)
+
+        config_file = {
+            'seed': 0,
+            'parent_dir': f'data/external/{dataset}_latent/',
+            'real_data_path': f'data/interim/{dataset}_latent/',
+            'model_type': 'mlp',
+            'num_numerical_features': num_feature_size,   # Set the number of numerical features here
+            'device': 'cpu',  
+            'model_params': {       # Change the denoising architecture here as per your liking
+                'd_in': 13, # Make this Input parameter
+                'num_classes': 2,
+                'is_y_cond': True,
+                'rtdl_params': {
+                    'd_layers': [
+                        128,
+                        512
+                    ],
+                    'dropout': 0.0
+                }
+            },
+            'diffusion_params': {
+                'num_timesteps': 1000,
+                'gaussian_loss_type': 'mse'
+            },
+            'train': {
+                'main': {
+                    'steps': 30000,
+                    'lr': 1.1510940031144828e-05,
+                    'weight_decay': 0.0,
+                    'batch_size': 4096
+                },
+                'T': {
+                    'seed': 0,
+                    'normalization': 'quantile',
+                    'num_nan_policy': '__none__',
+                    'cat_nan_policy': '__none__',
+                    'cat_min_frequency': '__none__',
+                    'cat_encoding': '__none__',
+                    'y_policy': 'default'
+                }
+            },
+            'sample': {
+                'num_samples': 5000,
+                'batch_size': 500,
+                'seed': 0
+            },
+            'eval': {
+                'type': {
+                    'eval_model': 'catboost',
+                    'eval_type': 'synthetic'
+                },
+                'T': {
+                    'seed': 0,
+                    'normalization': '__none__',
+                    'num_nan_policy': '__none__',
+                    'cat_nan_policy': '__none__',
+                    'cat_min_frequency': '__none__',
+                    'cat_encoding': '__none__',
+                    'y_policy': 'default'
+                }
+            }
+        }
+
+        with open(f"data/external/{dataset}_latent/config.toml", 'w') as toml_file:
+            toml.dump(config_file, toml_file)
+
+        command = [
+        "python", 
+        "tabddpm/scripts/pipeline.py", 
+        "--config", f"data/external/{dataset}_latent/config.toml", 
+        "--train", 
+        "--sample"
+        ]
+        print("Starting subprocess now...")
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stderr)
+    
+    if dataset == "bank_generate":
+        if trained_model is None:
+            raise ValueError("Model is not available. Ensure you have trained the model before generating data.")
+        generated_latent = np.load("data/external/bank_latent/X_num_unnorm.npy")
+        df = pd.DataFrame(generated_latent)
+        df.to_csv("data/external/bank_synth_latent.csv", index=False)
+        generated_data = trained_model.sample(5000, path="data/external/bank_synth_latent.csv")
+        generated_data.to_csv("data/external/bank_synth_data.csv", index=False)
+
+    # Diabetes Dataset
+    if dataset == "diabetes":
+        print("Setting up and running for Diabetes Data...")
+        # The default block of code to run if no dataset is specified
+        data = pd.read_csv("data/raw/diabetes.csv", sep=",")  # Raw Data
+        _, label = split_columns(data, save_path="data/interim/diabetes_no_label.csv", diabetes_data=True)
+        no_label_data = pd.read_csv("data/interim/diabetes_no_label.csv", sep=",")
+        model = TVAE(embedding_dim=no_label_data.shape[1], compress_dims=(32, 1024), decompress_dims=(1024, 32))
+        generate_and_save_latent(model, source="data/interim/diabetes_no_label.csv", path="data/processed/diabetes_latent.csv")
+        
+        latent_check = pd.read_csv("data/processed/diabetes_latent.csv")
+        print("CSV Read...")
+        combined_df = pd.concat([latent_check, label], axis=1)
+        combined_df.to_csv("data/processed/diabetes_latent_w_label.csv", index=False)
+
+        return model
+
+    if dataset == "diabetes_latent":
+        # This block will run only if the dataset argument is provided
+        data = pd.read_csv("data/processed/diabetes_latent_w_label.csv")
+        X = data.iloc[:, :-1].values
+        print("Debug1: Tabddpm latent feature", X.shape)
+        y = data.iloc[:, -1].values
+        print("Debug1: Tabddpm label latent feature", y.shape)
+        # Perform further processing on X and y as needed
+        print("Dataset processing completed.")
+
+        idx = np.arange(0, X.shape[0])
+        train_idx, test_idx = train_test_split(idx, test_size=0.3, random_state=42)
+        val_idx, test_idx = train_test_split(test_idx, test_size=0.5, random_state=42)
+        X_train, X_val, X_test = X[train_idx], X[val_idx], X[test_idx]
+        y_train, y_val, y_test = y[train_idx], y[val_idx], y[test_idx]
+        
+        print("Train, Test, Val Split Complete")
+        
+        train_size, val_size, test_size = X_train.shape[0], X_val.shape[0], X_test.shape[0]
+        print(train_size, val_size, test_size)
+
+        # Ensure the directory exists
         output_dir = f'data/interim/{dataset}/'
         os.makedirs(output_dir, exist_ok=True)
 
@@ -72,7 +235,8 @@ def main(dataset=None, trained_model=None):
         
         print("Interim Data Y Saved in .npy format for Tabddpm")
 
-        num_feature_size, cat_features_size = 13, 0 # These are default considering latent size of each client is 3 in VAE
+        # Make num_feature_size a input parameter
+        num_feature_size, cat_features_size = 8, 0 # These are default considering latent size of each client is 3 in VAE
         
         info_file = {
         "task_type": "binclass",
@@ -99,7 +263,7 @@ def main(dataset=None, trained_model=None):
             'num_numerical_features': num_feature_size,   # Set the number of numerical features here
             'device': 'cpu',  
             'model_params': {       # Change the denoising architecture here as per your liking
-                'd_in': 13,
+                'd_in': 8, # Make this Input parameter
                 'num_classes': 2,
                 'is_y_cond': True,
                 'rtdl_params': {
@@ -167,22 +331,28 @@ def main(dataset=None, trained_model=None):
         result = subprocess.run(command, capture_output=True, text=True)
         print(result.stderr)
     
-    if dataset == "generate":
+    if dataset == "diabetes_generate":
+        print("Generating synthetic data now...")
         if trained_model is None:
             raise ValueError("Model is not available. Ensure you have trained the model before generating data.")
-        generated_latent = np.load("data/external/bank_latent/X_num_unnorm.npy")
+        generated_latent = np.load("data/external/diabetes_latent/X_num_unnorm.npy")
         df = pd.DataFrame(generated_latent)
-        df.to_csv("data/external/bank_synth_latent.csv", index=False)
-        generated_data = trained_model.sample(5000, path="data/external/bank_synth_latent.csv")
-        generated_data.to_csv("data/external/bank_synth_data.csv", index=False)
+        df.to_csv("data/external/diabetes_synth_latent.csv", index=False)
+        generated_data = trained_model.sample(5000, path="data/external/diabetes_synth_latent.csv")
+        generated_data.to_csv("data/external/diabetes_synth_data.csv", index=False)
 
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to process data or run the dataset code.")
     parser.add_argument("--dataset", type=str, help="Specify the dataset to run, e.g., 'bank_latent'")
     args = parser.parse_args()
+    if args.dataset == 'bank':
+        trained_model = main(args.dataset)
+        main("bank_latent")
+        main("bank_generate", trained_model=trained_model)
+    elif args.dataset == 'diabetes':
+        trained_model = main(args.dataset)
+        main("diabetes_latent")
+        main("diabetes_generate", trained_model=trained_model)
 
-    trained_model = main(args.dataset)
-    main("bank_latent")
-    main("generate", trained_model=trained_model)
 
